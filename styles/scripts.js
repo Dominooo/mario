678ebf3511dda5d53d133c30aaf8fbe11c128949
Dominooo <DevinOlsen87@gmail.com>
2015-10-22 02:58:19 -0600

    validation added

--- styles/scripts.js
+++ styles/scripts.js
@@ -27,38 +27,130 @@ var app = angular.module("app", ["firebase"]);
           $scope.playedLevels = $firebaseArray(playedRef);
           
 
-          // Login stuff
-//          var ref = new Firebase("https://mariomaker.firebaseio.com");
- //         ref.authWithPassword({
-//            email    : $scope.email,
-//            password : $scope.password
-//          }, function(error, authData) {
-//            if (error) {
-//              console.log("Login Failed!", error);
-//            } else {
-//              console.log("Authenticated successfully with payload:", authData);
-//            }
- //         });
 
           
           //ADD NEW LEVEL METHOD
           $scope.addLevel = function(e) {
           $scope.newLevel = $scope.levelPiece1.toUpperCase() + " - " +  $scope.levelPiece2.toUpperCase()  + " - " + $scope.levelPiece3.toUpperCase()  + " - " + $scope.levelPiece4.toUpperCase();
+          $scope.newLevel = $scope.newLevel.toString();
+          $scope.checkPass = true;
+
             //WHEN SUBMIT IS PRESSED
             if ($scope.name && $scope.levelPiece2 == 0000 && $scope.levelPiece1.length == 4 && $scope.levelPiece2.length == 4 && $scope.levelPiece3.length == 4 && $scope.levelPiece4.length == 4) {
-              $scope.unplayedLevels.$add({ name: $scope.name, level: $scope.newLevel });
-              //RESET LEVEL BOXES
-              $scope.levelPiece1 = "";
-              $scope.levelPiece2 = "";
-              $scope.levelPiece3 = "";
-              $scope.levelPiece4 = "";
+              console.log("scope.newLevel is " + $scope.newLevel);
+              makeArrays();
             }
             
             else {
-               alert("Something is wrong. Please double check that you have entered a name and a valid level code.");
+               alert("Something is wrong. Please double check that you have entered a name and a real level code.");
             }
             
           };
+
+
+
+
+            //Populate arrays to check against later
+
+            function makeArrays() {
+
+              var playedList = new Firebase("https://mariomaker.firebaseio.com/trent/played");
+              var played = $firebaseArray(playedList);
+              $scope.awefulPlayedLevelArray = [];
+              played.$loaded()
+                .then(function(){
+                    angular.forEach(played, function(data) {
+                        $scope.awefulPlayedLevelArray.push(data.level);
+                        
+                    })
+                    //console.log($scope.awefulPlayedLevelArray);
+                    //checkIfLevelExistsInPlayed($scope.newLevel);
+                });
+
+              var unplayedList = new Firebase("https://mariomaker.firebaseio.com/trent/unplayed");
+              var unplayed = $firebaseArray(unplayedList);
+              $scope.awefulUnplayedLevelArray = [];
+              unplayed.$loaded()
+                .then(function(){
+                    angular.forEach(unplayed, function(data) {
+                        $scope.awefulUnplayedLevelArray.push(data.level);
+                        
+                    })
+                    //console.log($scope.awefulUnplayedLevelArray);
+                    checkIfLevelExistsInUnplayed($scope.newLevel);
+                });
+
+
+            }
+
+
+            // Tests to see if level exists already in unplayed
+            
+
+            function checkIfLevelExistsInUnplayed(newLevel) {
+              console.log("checking unplayed")
+              
+              console.log("checkPass is " + $scope.checkPass)
+              for (var i = 0; i < $scope.awefulUnplayedLevelArray.length; i++) {
+                  if (newLevel === $scope.awefulUnplayedLevelArray[i]){
+                    console.log("match")
+                    $scope.checkPass = false;
+                    console.log("checkPass is " + $scope.checkPass)
+                    levelExistsCallback(newLevel);
+                    break;
+                  }
+              }
+                
+                if ($scope.checkPass === true){
+                  console.log("checkPass is " + $scope.checkPass)
+                  checkIfLevelExistsInPlayed(newLevel);
+                }
+                
+            }
+
+            // Tests to see if level exists already in played
+            function checkIfLevelExistsInPlayed(newLevel) {
+
+                console.log("checking played")
+                
+                console.log("checkPass is " + $scope.checkPass)
+              
+                for (var i = 0; i < $scope.awefulPlayedLevelArray.length; i++) {
+                    if (newLevel === $scope.awefulPlayedLevelArray[i]){
+                      console.log("match")
+                      $scope.checkPass = false;
+                      console.log("checkPass is " + $scope.checkPass)
+                      levelExistsCallback(newLevel);
+                      break;
+                    }
+                }
+                  
+                  if ($scope.checkPass === true){
+                  console.log("checkPass is " + $scope.checkPass)
+                  levelExistsCallback(newLevel);
+              }
+            }
+
+
+
+           // What to do after we check if level exists
+           function levelExistsCallback(newLevel) {
+            console.log("Deciding what message to give")
+              if ($scope.checkPass === false) {
+                alert('Level "' + newLevel + '"" was not added. No duplicates!');
+              } else {
+                $scope.unplayedLevels.$add({ name: $scope.name, level: $scope.newLevel });
+                alert('Level "' + newLevel + '"" has been submitted!');
+                //RESET LEVEL BOXES
+                $scope.levelPiece1 = "";
+                $scope.levelPiece2 = "";
+                $scope.levelPiece3 = "";
+                $scope.levelPiece4 = "";
+              }
+            }   
+            
+            
+
           
           
           // GET A RANDOM LEVEL
@@ -81,7 +173,7 @@ var app = angular.module("app", ["firebase"]);
           
           // MAKE SURE YOU CANT ADD A LEVEL THAT ALREADY EXISTS
           
-          // REMOVE A LEVEL FROM THE POOL AFTER IT HAS BEEN PLAYED. POSSIBLY SAVE TO NEW "PREVIOUSLY PLAYED" OBJECT SO WE CAN CHECK AGAINST THAT.
+          
           
 
           
